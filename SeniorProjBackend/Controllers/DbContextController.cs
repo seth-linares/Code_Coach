6e8fba53810993a74b730747e5740a4d214a1be4
sethlinares <sethlinares1@gmail.com>
2024-04-05 21:00:48 -0400

    update

--- SeniorProjBackend/Controllers/DbContextController.cs
+++ SeniorProjBackend/Controllers/DbContextController.cs
@@ -1,4 +1,9 @@
-﻿namespace SeniorProjBackend.Controllers
+﻿// Controller need the MVC package
+using Microsoft.AspNetCore.Mvc;
+using SeniorProjBackend.Data;
+using
+
+namespace SeniorProjBackend.Controllers
 {
     public class DbContextController
     {
