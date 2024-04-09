dfb15f5f223d122c66be65c106a8016a37c6f4a5
sethlinares <sethlinares1@gmail.com>
2024-04-09 18:29:43 -0400

    Update Problem.cs

--- SeniorProjBackend/Data/Problem.cs
+++ SeniorProjBackend/Data/Problem.cs
@@ -6,7 +6,7 @@
          * 2.  **Problems Table**:
                 *   `ProblemID` (Primary Key, INT): Unique identifier for each problem.
                 *   `Title` (VARCHAR): Title of the problem.
-                *   `Description` (VARCHAR(MAX)): Detailed description of the problem.
+                *   `Description` (NVARCHAR(MAX)): Detailed description of the problem.
                 *   `DifficultyScore` (INT): ELO/Kyu score indicating the problem's difficulty.
                 *   `IsActive` (Boolean): Indicates whether the problem is active and available to users.
                 *   `LastModifiedDate` (DateTime): Date and time when the problem was last modified.
@@ -15,7 +15,7 @@
 
         public int ProblemID { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; } // VARCHAR MAX
+        public string Description { get; set; } // NVARCHAR MAX
         public int DifficultyScore { get; set; }
         public bool IsActive { get; set; }
         public DateTime LastModifiedDate { get; set; }
