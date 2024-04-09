b5243549378f806feaa2e6f4cfdebba48a40eb3b
sethlinares <sethlinares1@gmail.com>
2024-04-08 20:24:30 -0400

    Update OurDbContext.cs

--- SeniorProjBackend/Data/OurDbContext.cs
+++ SeniorProjBackend/Data/OurDbContext.cs
@@ -4,6 +4,17 @@ namespace SeniorProjBackend.Data
 {
     public class OurDbContext : DbContext 
     {
+        // DbSet for each table
+        public DbSet<User> Users { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Problem> Problems { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<ProblemCategory> ProblemCategories { get; set; }
+        public DbSet<ProblemLanguage> ProblemLanguages { get; set; }
+
+
         public OurDbContext(DbContextOptions<OurDbContext> options) : base(options)
         {
 
