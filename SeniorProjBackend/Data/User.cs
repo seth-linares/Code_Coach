2a49b7e397983379cca1ed60feddf05c6937d61f
sethlinares <sethlinares1@gmail.com>
2024-04-09 20:58:13 -0400

    Update User.cs

--- SeniorProjBackend/Data/User.cs
+++ SeniorProjBackend/Data/User.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace SeniorProjBackend.Data
+﻿namespace SeniorProjBackend.Data
 {
     public class User
     {
@@ -14,11 +12,11 @@ namespace SeniorProjBackend.Data
             *   `SecretKey` (VARCHAR, optional): Encrypted  secret key used for generating 2FA codes. Null or empty if 2FA is not enabled.
             *   `TotalScore` (INT): Accumulated score from problem-solving activities.
             *   `Bio` (VARCHAR, optional): Short biography or user description.
-            *   `ProfilePictureURL` (VARCHAR, optional): URL to the user's profile picture.
+            *   `ProfilePictureURL` (VARCHAR): URL to the user's profile picture, has default value
             *   `RegistrationDate` (DateTime): Date and time when the user registered.
             *   `LastActiveDate` (DateTime): Date and time when the user was last active.
-            *   `Rank` (VARCHAR, optional): User's rank, calculated from `TotalScore`.
-            *   `RankIconURL` (VARCHAR, optional): URL to an icon/image representing the user's rank.
+            *   `Rank` (VARCHAR): User's rank, calculated from `TotalScore`.
+            *   `RankIconURL` (VARCHAR): URL to an icon/image representing the user's rank, has default value
             *   `ActiveStreak` (INT, optional): Number of consecutive days the user has been active.
         */
 
@@ -27,14 +25,14 @@ namespace SeniorProjBackend.Data
         public string PasswordHash { get; set; }
         public string EmailAddress { get; set; }
         public bool TwoFactorEnabled { get; set; }
-        public string? SecretKey { get; set; }
+        public string? SecretKey { get; set; } // encrypted secret key for 2FA
         public int TotalScore { get; set; }
         public string? Bio {  get; set; }
-        public string? ProfilePictureURL { get; set; }
+        public string ProfilePictureURL { get; set; }
         public DateTime RegistrationDate { get; set; }
         public DateTime LastActiveDate { get; set; }
-        public string Rank { get; set; }
-        public string RankIconURL { get; set; }
+        public string Rank { get; set; } 
+        public string RankIconURL { get; set; } // For icons we need to have a default url for each in the fluent api
         public int ActiveStreak { get; set; }
 
         // Navigation properties
