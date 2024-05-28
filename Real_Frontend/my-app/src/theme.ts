0a1efee8dd0dffff0e706e167bfa1c5a092a7a26
sethlinares <sethlinares1@gmail.com>
2024-05-28 19:50:48 -0400

    Create theme.ts

---
+++ Real_Frontend/my-app/src/theme.ts
@@ -0,0 +1,17 @@
+// Seems like we can configure MUI theme here?
+
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+export default theme;
