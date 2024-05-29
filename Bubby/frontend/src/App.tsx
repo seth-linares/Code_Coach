851232f779a0b418fb9498ac8fbd2f35f391ca90
sethlinares <sethlinares1@gmail.com>
2024-05-28 21:36:20 -0400

    Update App.tsx

--- Bubby/frontend/src/App.tsx
+++ Bubby/frontend/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline text-orange-300">
       Hello world!
       </h1>
       <header className="App-header">
