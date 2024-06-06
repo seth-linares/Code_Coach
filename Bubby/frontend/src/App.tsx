09df025df5d4ac62eea6927e6fd4125dbc31c833
sethlinares <sethlinares1@gmail.com>
2024-06-05 23:43:18 -0400

    basic pages

--- Bubby/frontend/src/App.tsx
+++ Bubby/frontend/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from "./pages/LoginPage";
+import HelpPage from "./pages/HelpPage";
 
 function App() {
   return (
+
+
     <div className="App">
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path={"/help"} element={<HelpPage />} />
+            </Routes>
+        </Router>
       <h1 className="text-3xl font-bold underline text-orange-300">
       Hello world!
       </h1>
---
+++ Bubby/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "react-router-dom";
+import App from "../App";
+
+function LoginPage() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default LoginPage;
