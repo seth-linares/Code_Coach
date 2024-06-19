d2b5bfca46b6044d0a87a114f229ed278083a2a2
sethlinares <sethlinares1@gmail.com>
2024-06-18 22:49:13 -0400

    Update tailwind.config.ts

--- RealBubbyFrontend/real_cool_frontend/tailwind.config.ts
+++ RealBubbyFrontend/real_cool_frontend/tailwind.config.ts
@@ -17,14 +17,32 @@ const config: Config = {
   ],
   daisyui: {
     themes: [
-      "dark",   // included dark theme
-      "dracula", // custom or pre-configured theme names
-      "night",
-      "dim",
-      "business",
-      "synthwave"
+      {
+        mytheme: {
+          "primary": "#0071ff",
+          "primary-content": "#000416",
+          "secondary": "#00e2ff",
+          "secondary-content": "#001216",
+          "accent": "#fb7185",
+          "accent-content": "#150406",
+          "neutral": "#374151",
+          "neutral-content": "#d3d6da",
+          "base-100": "#1d2735",
+          "base-200": "#18202d",
+          "base-300": "#131a25",
+          "base-content": "#cccfd3",
+          "info": "#c084fc",
+          "info-content": "#0e0616",
+          "success": "#16a34a",
+          "success-content": "#000a02",
+          "warning": "#facc15",
+          "warning-content": "#150f00",
+          "error": "#df1e39",
+          "error-content": "#ffd8d6",
+        },
+      },
     ],
-    darkTheme: "dracula",
+    darkTheme: "mytheme",
     base: true,
     styled: true,
     utils: true,
@@ -33,4 +51,5 @@ const config: Config = {
     themeRoot: ":root",
   },
 };
+
 export default config;
