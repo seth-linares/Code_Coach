b09fa1b11b918386e9f842e4decb1f6eeef0062f
sethlinares <sethlinares1@gmail.com>
2024-06-18 21:12:09 -0400

    Update tailwind.config.ts

--- RealBubbyFrontend/real_cool_frontend/tailwind.config.ts
+++ RealBubbyFrontend/real_cool_frontend/tailwind.config.ts
@@ -2,17 +2,35 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // This line covers all subdirectories
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Covers all subdirectories
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      "dark",   // included dark theme
+      "dracula", // custom or pre-configured theme names
+      "night",
+      "dim",
+      "business",
+      "synthwave"
+    ],
+    darkTheme: "dracula",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
 };
 export default config;
