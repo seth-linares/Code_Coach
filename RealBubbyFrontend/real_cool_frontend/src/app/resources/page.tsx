a4b86317191f73622b55b50fd315b2a3c744c3d4
sethlinares <sethlinares1@gmail.com>
2024-06-18 21:12:02 -0400

    Create page.tsx

---
+++ RealBubbyFrontend/real_cool_frontend/src/app/resources/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Link href="/" className="btn font-bold text-xl">
+                Home
+            </Link>
+            <h1>Resource Bubby!</h1>
+
+
+        </main>
+    );
+}
