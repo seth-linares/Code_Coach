0fa29eed6aebe75f5631d4691e526abf77fd114e
sethlinares <sethlinares1@gmail.com>
2024-06-18 22:49:08 -0400

    Update HeroSection.tsx

--- RealBubbyFrontend/real_cool_frontend/src/components/HeroSection.tsx
+++ RealBubbyFrontend/real_cool_frontend/src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export const HeroSection = () => {
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: `url('https://i.pinimg.com/originals/0d/10/d2/0d10d2fe48a7956a4fdc9f7251132236.gif')`}}>
-            <div className="hero-overlay bg-black bg-opacity-70"></div>
+            <div className="hero-overlay bg-black bg-opacity-70"></div> {/*lessen image to make text readable*/}
             <div className="hero-content text-center text-neutral-content">
                 <div className="min-w-64 px-6 py-12">
                     <h1 className="mb-10 text-8xl font-extrabold text-white" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
