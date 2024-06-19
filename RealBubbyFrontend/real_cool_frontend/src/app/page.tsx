0b3a675285fd3f7284a5a94ac197d043675016f3
sethlinares <sethlinares1@gmail.com>
2024-06-18 22:49:03 -0400

    Update page.tsx

--- RealBubbyFrontend/real_cool_frontend/src/app/page.tsx
+++ RealBubbyFrontend/real_cool_frontend/src/app/page.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-import {Navbar} from "../components/Navbar";
-import {Footer} from "../components/Footer";
-import {HeroSection} from "../components/HeroSection";
+
+import {Navbar} from "@/components/Navbar";
+import {HeroSection} from "@/components/HeroSection";
+import {FeaturesSection} from "@/components/FeaturesSection";
+import {Footer} from "@/components/Footer";
+import {HowItWorksSection} from "@/components/HowItWorksSection";
 
 export default function Home() {
   return (
-    <main data-theme={"dracula"}>
+    <main className={"bg-base-100"}>
       <Navbar/>
       <HeroSection/>
-      <Footer/>
+      <FeaturesSection/>
+      <HowItWorksSection/>
+      {/*<Footer/>*/}
     </main>
   );
 }
