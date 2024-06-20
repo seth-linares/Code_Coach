768c0749050cd9c3da2eb11538269b7e776c7be3
sethlinares <sethlinares1@gmail.com>
2024-06-20 14:44:51 -0400

    Update UsersController.cs

--- SeniorProjBackend/Controllers/UsersController.cs
+++ SeniorProjBackend/Controllers/UsersController.cs
@@ -9,20 +9,28 @@ using Microsoft.EntityFrameworkCore;
 using SeniorProjBackend.Data;
 using SeniorProjBackend.DTOs;
 using SeniorProjBackend.Encryption;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace SeniorProjBackend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class UsersController : ControllerBase
     {
         private readonly ITokenService _tokenService;
         private readonly OurDbContext _context;
+        private readonly ILogger _logger;
+        private readonly PasswordHasher<User> _passwordHasher;
 
-        public UsersController(OurDbContext context, ITokenService tokenService)
+        public UsersController(OurDbContext context, ITokenService tokenService, ILogger logger, PasswordHasher<User> passwordHasher)
         {
             _context = context;
             _tokenService=tokenService;
+            _logger = logger;
+            _passwordHasher = passwordHasher;
+
         }
 
         // GET: api/Users
@@ -34,8 +42,7 @@ namespace SeniorProjBackend.Controllers
 
         // GET: api/Users/5
         [HttpGet("{id}")]
-        [Authorize] // Apply JWT authentication middleware
-        public async Task<ActionResult<User>> GetUser(int id)
+        public async Task<ActionResult<User>> GetUserById(int id)
         {
             var user = await _context.Users.FindAsync(id);
 
@@ -117,39 +124,51 @@ namespace SeniorProjBackend.Controllers
             bool userExists = await _context.Users.AnyAsync(u => u.Username == userDto.Username);
             if (userExists)
             {
-                return BadRequest("Username is already taken.");
+                return BadRequest(new { message = "Username is already taken." });
             }
 
             // check if the email address is already taken
             bool emailExists = await _context.Users.AnyAsync(u => u.EmailAddress == userDto.EmailAddress);
             if (emailExists)
             {
-                return BadRequest("Email Address is already taken.");
+                return BadRequest(new { message = "Email Address is already taken." });
             }
-
-            // hash the password
-            var hasher = new PasswordHasher();
-            var hashedPassword = hasher.HashPassword(userDto.Password);
 
             // create a new User entity
             var newUser = new User
             {
                 Username = userDto.Username,
-                PasswordHash = hashedPassword, // store the hashed password
+                PasswordHash = "temp_pass", // store the hashed password
                 EmailAddress = userDto.EmailAddress
             };
 
-            // add user to the database context
-            _context.Users.Add(newUser);
+            // hash the password
+
+            var hashedPassword = _passwordHasher.HashPassword(newUser, userDto.Password);
+
+            newUser.PasswordHash = hashedPassword;
 
 
-            await _context.SaveChangesAsync();
+            await using var transaction = await _context.Database.BeginTransactionAsync();
+            try
+            {
+                _context.Users.Add(newUser);
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                return StatusCode(500, new { message = $"An error occured while creating the user: {ex.Message}" });
+            }
 
 
 
             var token = _tokenService.GenerateToken(newUser); // Use the injected _tokenService
 
-            return Ok(new { token }); // Return the JWT in the response
+            // Return the JWT in the response
+            return CreatedAtAction(nameof(GetUserById), new { id = newUser.UserID }, new { token }); 
         }
 
         // POST: api/Users/Login
@@ -160,24 +179,41 @@ namespace SeniorProjBackend.Controllers
             var user = await _context.Users.SingleOrDefaultAsync(u => u.Username == userDto.Username);
             if (user == null)
             {
-                return Unauthorized("Invalid username or password.");
+                return Unauthorized(new { message = "Invalid username or password." });
             }
 
             // verify the password hash
-            var hasher = new PasswordHasher();
-            var passwordIsValid = hasher.VerifyPassword(user.PasswordHash, userDto.Password);
-            if (!passwordIsValid)
+            var passwordIsValid = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, userDto.Password);
+            if (passwordIsValid == PasswordVerificationResult.Failed)
             {
-                return Unauthorized("Invalid username or password.");
+                return Unauthorized(new { message = "Invalid username or password." });
+            }
+
+
+            /*
+             * If we know the password used in the Dto was legit but old, we can just
+             * take it from the Dto and then hash it and store the new password
+             */
+            else if (passwordIsValid == PasswordVerificationResult.SuccessRehashNeeded)
+            {
+                user.PasswordHash = _passwordHasher.HashPassword(user, userDto.Password);
             }
 
             user.LastActiveDate =  DateTime.UtcNow;
+
+
+
+            await _context.SaveChangesAsync();
+
+
+
+
 
             // generate a JWT
             var token = _tokenService.GenerateToken(user);
 
             // Return the JWT in the response
-            return Ok(new { token = token });
+            return Ok(new { token });
         }
 
 
