d0fcd2109cd1e7fa6eaf39505a1f169940c470f6
sethlinares <sethlinares1@gmail.com>
2024-07-19 22:58:25 -0400

    Update AIConversationsController.cs

--- SeniorProjBackend/Backend_Files/Controllers/AIConversationsController.cs
+++ SeniorProjBackend/Backend_Files/Controllers/AIConversationsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,7 @@ using SeniorProjBackend.Middleware;
 
 namespace SeniorProjBackend.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AIConversationsController : ControllerBase
