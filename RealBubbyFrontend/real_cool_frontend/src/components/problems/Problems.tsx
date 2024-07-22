49b304f6a0350e38ddd6414cb1060eaf553783af
sethlinares <sethlinares1@gmail.com>
2024-07-22 15:53:11 -0400

    Update Problems.tsx

--- RealBubbyFrontend/real_cool_frontend/src/components/problems/Problems.tsx
+++ RealBubbyFrontend/real_cool_frontend/src/components/problems/Problems.tsx
@@ -1,48 +1,35 @@
+// src/app/problems/[id]/page.tsx
+
 "use client"
 
-import { useRef, useState } from "react";
-import Editor, { OnMount } from "@monaco-editor/react";
-
-const languageMap: { [key: string]: string } = { 'Python': 'python', 'C++': 'cpp', 'C#': 'csharp' };
+import { useParams } from "next/navigation"
+import CodeEditor from "@/components/problems/CodeEditor";
+import useProblemDetails from "@/hooks/useProblemDetails";
 
 export function Problems() {
-    const [selectedLanguage, setSelectedLanguage] = useState("Python");
-    const editorRef = useRef<any>(null);
+    const params = useParams();
+    const probID = parseInt(params.id as string, 10);
+    const { problemDetails, loading, error } = useProblemDetails(probID);
 
-    const handleEditorDidMount: OnMount = (editor, monaco) => {
-        editorRef.current = editor;
-    };
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+    if (!problemDetails) return <div>No problem details found</div>;
+
+    console.log(`DESCRIPTION: ${problemDetails.description}`);
 
     return (
         <div className="flex-grow bg-blend-color flex flex-col md:flex-row p-4 gap-4">
             <div className="w-full md:w-1/2 flex flex-col">
-                <div className="flex-grow">
-                    <Editor
-                        height="60vh"
-                        language={languageMap[selectedLanguage]}
-                        theme="vs-dark"
-                        onMount={handleEditorDidMount}
-                        className="rounded-t-lg"
-                    />
-                </div>
-                <div className="flex justify-start items-center mt-2">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-sm m-1">{selectedLanguage}</div>
-                        <ul tabIndex={0}
-                            className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                            <li><a onClick={() => setSelectedLanguage('Python')}>Python</a></li>
-                            <li><a onClick={() => setSelectedLanguage('C++')}>C++</a></li>
-                            <li><a onClick={() => setSelectedLanguage('C#')}>C#</a></li>
-                        </ul>
-                    </div>
-                    <button className="btn btn-sm ml-2">Submit</button>
-                </div>
+                <CodeEditor
+                    problemId={probID}
+                    languageDetails={problemDetails.languageDetails}
+                />
             </div>
 
             <div className="w-full md:w-1/2 flex flex-col">
                 <div className="bg-base-100 p-4 rounded-lg mb-4">
-                    <h1 className="text-2xl font-bold mb-2 text-center">Title Here</h1>
-                    <p>Description Here</p>
+                    <h1 className="text-2xl font-bold mb-2 text-center">{problemDetails.title}</h1>
+                    <pre className="whitespace-pre-wrap">{problemDetails.description}</pre>
                 </div>
 
                 <div className="flex-grow flex flex-col">
