f2a7f811ad1f9c93b1424fec0c95b06e528279a5
sethlinares <sethlinares1@gmail.com>
2024-07-22 15:52:54 -0400

    Update page.tsx

--- RealBubbyFrontend/real_cool_frontend/src/app/profile/page.tsx
+++ RealBubbyFrontend/real_cool_frontend/src/app/profile/page.tsx
@@ -1,8 +1,8 @@
 // src/app/profile/page.tsx
-'use client';
 
 import dynamic from 'next/dynamic';
 import AuthWrapper from "@/components/common/AuthWrapper";
+import { Footer } from '@/components/common/Footer';
 
 const ProfilePage = dynamic(() => import('@/components/profile/ProfilePage'), {
     ssr: false,
@@ -10,12 +10,14 @@ const ProfilePage = dynamic(() => import('@/components/profile/ProfilePage'), {
 });
 
 export default function ProfileWrapper() {
-    return(
+    return (
         <AuthWrapper requireAuth={true}>
-            <div className="bg-gray-900">
-                <ProfilePage />
+            <div className="bg-gray-900 min-h-screen flex flex-col">
+                <div className="flex-grow">
+                    <ProfilePage />
+                </div>
+                <Footer/>
             </div>
-
         </AuthWrapper>
     );
 }
