398e425e0b0991e6918490ed6aa879417fc5ee1f
sethlinares <sethlinares1@gmail.com>
2024-07-22 15:53:14 -0400

    Update APIKeyModal.tsx

--- RealBubbyFrontend/real_cool_frontend/src/components/profile/APIKeyModal.tsx
+++ RealBubbyFrontend/real_cool_frontend/src/components/profile/APIKeyModal.tsx
@@ -1,17 +1,29 @@
+// src/components/profile/APIKeyModal.tsx
+
 import React from 'react';
 import { APIKeyModalProps } from "@/types";
 
-const APIKeyModal: React.FC<APIKeyModalProps> = ({
-                                                     isOpen,
-                                                     onClose,
-                                                     title,
-                                                     keyName,
-                                                     keyValue,
-                                                     onKeyNameChange,
-                                                     onKeyValueChange,
-                                                     onSubmit,
-                                                     submitText
-                                                 }) => {
+const APIKeyModal: React.FC<APIKeyModalProps> = React.memo(({
+    isOpen,
+    onClose,
+    title,
+    keyName,
+    keyValue,
+    onKeyNameChange,
+    onKeyValueChange,
+    onSubmit,
+    submitText
+}) => {
+
+
+    const handleKeyNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onKeyNameChange(e.target.value);
+    };
+
+    const handleKeyValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onKeyValueChange(e.target.value);
+    };
+
     return (
         <div className={`modal ${isOpen ? 'modal-open' : ''}`}>
             <div className="modal-box">
@@ -21,14 +33,14 @@ const APIKeyModal: React.FC<APIKeyModalProps> = ({
                     placeholder="Key Name"
                     className="input input-bordered w-full mt-4"
                     value={keyName}
-                    onChange={(e) => onKeyNameChange(e.target.value)}
+                    onChange={handleKeyNameChange}
                 />
                 <input
                     type="text"
                     placeholder="Key Value"
                     className="input input-bordered w-full mt-2"
                     value={keyValue}
-                    onChange={(e) => onKeyValueChange(e.target.value)}
+                    onChange={handleKeyValueChange}
                 />
                 <div className="modal-action">
                     <button className="btn" onClick={onClose}>Cancel</button>
@@ -37,6 +49,6 @@ const APIKeyModal: React.FC<APIKeyModalProps> = ({
             </div>
         </div>
     );
-};
+});
 
 export default APIKeyModal;
